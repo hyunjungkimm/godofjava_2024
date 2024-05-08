892faf2d74675d12e629e7ea6c202fdced856503
kimhyeonjeong <khjjung1124@gmail.com>
2024-05-08 14:13:09 +0900

    [chapter03] 20240508 자바의신

---
+++ src/chapter02/Profile.java
@@ -0,0 +1,30 @@
+package chapter02;
+
+public class Profile {
+    String name;
+    int age;
+
+    public void setName(String str){
+        name = str;
+    }
+
+    public void printName(){
+        System.out.println("My name is " + name);
+    }
+
+    public void setAge(int val){
+        age = val;
+    }
+
+    public void printAge(){
+        System.out.println("My age is " + age);
+    }
+
+    public static void main(String[] args) {
+        Profile profile = new Profile();
+        profile.setName("Min");
+        profile.setAge(20);
+        profile.printName();
+        profile.printAge();
+    }
+}
